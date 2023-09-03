42d0905c9ed6820bf82c12cd577f13e02bdc6c63
youHappyOK <luojin_mail@163.com>
2023-09-03 17:25:53 +0800

    gpu训练

--- detect_pic_test.py
+++ detect_pic_test.py
@@ -1,10 +1,20 @@
+import torch
+
 from ultralytics import YOLO
 import cv2
 # 加载预训练模型
 model = YOLO("yolov8n.pt", task='detect')
+
+# 检查是否有可用的 GPU 设备
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# 在检测阶段使用gpu
+model = model.to(device)
+
 # model = YOLO("yolov8n.pt") task参数也可以不填写，它会根据模型去识别相应任务类别
 # 检测图片
 results = model("./ultralytics/assets/img_3.png")
+
+
 res = results[0].plot()
 cv2.imshow("YOLOv8 Inference", res)
 cv2.waitKey(0)
