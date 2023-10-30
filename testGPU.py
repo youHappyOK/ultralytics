e5a76ddab479b6fb82d76ed4780c9b84e501ec7f
youHappyOK <luojin_mail@163.com>
2023-10-30 23:31:38 +0800

    置信度和iou

--- testGPU.py
+++ testGPU.py
@@ -9,5 +9,7 @@ print(torch.cuda.device_count())
 # 查看对应gpu的cuda版本号
 print(torch.backends.cudnn.version())
 print(torch.version.cuda)
+# gpu名字
+print(torch.cuda.get_device_name())
 # 退出python
 quit()
