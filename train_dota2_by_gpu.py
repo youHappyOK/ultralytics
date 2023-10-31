c8b63340448d0b76197ebdf275719b5f8e49bf4e
youHappyOK <luojin_mail@163.com>
2023-10-31 22:00:08 +0800

    置信度和iou

---
+++ train_dota2_by_gpu.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO('yolov8n.pt')
+model.info()
+
+if __name__ == '__main__':
+
+    # Train the model
+    results = model.train(data=r'C:\Users\Administrator\PycharmProjects\ultralytics\datasets\Dota 2.v2i.yolov8\data.yaml', epochs=500, imgsz=640)
+    # 指定gpu训练
+    # results = model.train(data=r'C:\Users\Administrator\PycharmProjects\ultralytics\ultralytics\cfg\datasets\airplane.yaml', epochs=50, imgsz=640, device=0)
